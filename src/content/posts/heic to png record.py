692302ed6c3af753f236c7c20d181c0756296fc5
Shu <93468573+shu-sz9922@users.noreply.github.com>
2025-09-06 12:31:21 -0400

    Add files via upload

---
+++ src/content/posts/heic to png record.py
@@ -0,0 +1,71 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>     pip install pillow_heif
+...     
+SyntaxError: unexpected indent
+>>> pip install pillow_heif
+SyntaxError: invalid syntax
+>>> 
+Warning (from warnings module):
+  File "E:/OneDrive - The Pennsylvania State University/#25 Fall/turn multi heic to png.py", line 6
+    heic_directory = "E:\OneDrive - The Pennsylvania State University\#25 Fall"
+SyntaxWarning: invalid escape sequence '\O'
+>>> 
+Warning (from warnings module):
+  File "E:/OneDrive - The Pennsylvania State University/#25 Fall/turn multi heic to png.py", line 8
+    png_directory = "E:\OneDrive - The Pennsylvania State University\#25 Fall"
+SyntaxWarning: invalid escape sequence '\O'
+>>> 
+= RESTART: E:/OneDrive - The Pennsylvania State University/#25 Fall/turn multi heic to png.py
+Converted 20250902_204002122_iOS.heic to 20250902_204002122_iOS.png
+Converted 20250902_204225138_iOS.heic to 20250902_204225138_iOS.png
+Converted 20250902_204323750_iOS.heic to 20250902_204323750_iOS.png
+Converted 20250902_204547589_iOS.heic to 20250902_204547589_iOS.png
+Converted 20250902_205731805_iOS.heic to 20250902_205731805_iOS.png
+Converted 20250902_210139888_iOS.heic to 20250902_210139888_iOS.png
+Converted 20250902_210212702_iOS.heic to 20250902_210212702_iOS.png
+Converted 20250902_213356050_iOS.heic to 20250902_213356050_iOS.png
+Converted 20250902_213359756_iOS.heic to 20250902_213359756_iOS.png
+Converted 20250902_215419348_iOS.heic to 20250902_215419348_iOS.png
+Converted 20250902_220600631_iOS.heic to 20250902_220600631_iOS.png
+Converted 20250902_224859545_iOS.heic to 20250902_224859545_iOS.png
+Converted 20250903_035835984_iOS.heic to 20250903_035835984_iOS.png
+Converted 20250903_182341322_iOS.heic to 20250903_182341322_iOS.png
+Converted 20250903_182905711_iOS.heic to 20250903_182905711_iOS.png
+Converted 20250903_182936307_iOS.heic to 20250903_182936307_iOS.png
+Converted 20250903_183749827_iOS.heic to 20250903_183749827_iOS.png
+Converted 20250903_183955233_iOS.heic to 20250903_183955233_iOS.png
+Converted 20250903_184139036_iOS.heic to 20250903_184139036_iOS.png
+Converted 20250903_184739311_iOS.heic to 20250903_184739311_iOS.png
+Converted 20250903_185014216_iOS.heic to 20250903_185014216_iOS.png
+Converted 20250903_185434346_iOS.heic to 20250903_185434346_iOS.png
+Converted 20250903_185540224_iOS.heic to 20250903_185540224_iOS.png
+Converted 20250903_204741840_iOS.heic to 20250903_204741840_iOS.png
+Converted 20250903_204827216_iOS.heic to 20250903_204827216_iOS.png
+Converted 20250903_221042112_iOS.heic to 20250903_221042112_iOS.png
+Converted 20250904_003225413_iOS.heic to 20250904_003225413_iOS.png
+Converted 20250904_003230226_iOS.heic to 20250904_003230226_iOS.png
+Converted 20250904_012920783_iOS.heic to 20250904_012920783_iOS.png
+Converted 20250904_012921176_iOS.heic to 20250904_012921176_iOS.png
+Converted 20250904_013339461_iOS.heic to 20250904_013339461_iOS.png
+Converted 20250904_130015131_iOS.heic to 20250904_130015131_iOS.png
+Converted 20250904_150118840_iOS.heic to 20250904_150118840_iOS.png
+Converted 20250904_153430121_iOS.heic to 20250904_153430121_iOS.png
+Converted 20250904_170709090_iOS.heic to 20250904_170709090_iOS.png
+Converted 20250905_120433390_iOS.heic to 20250905_120433390_iOS.png
+Converted 20250905_175534101_iOS.heic to 20250905_175534101_iOS.png
+Converted 20250905_184029039_iOS.heic to 20250905_184029039_iOS.png
+Converted 20250905_184824815_iOS.heic to 20250905_184824815_iOS.png
+Converted 20250905_185015206_iOS.heic to 20250905_185015206_iOS.png
+Converted 20250905_190130701_iOS.heic to 20250905_190130701_iOS.png
+Converted 20250905_190242016_iOS.heic to 20250905_190242016_iOS.png
+Converted 20250905_190352585_iOS.heic to 20250905_190352585_iOS.png
+Converted 20250905_190428615_iOS.heic to 20250905_190428615_iOS.png
+Converted 20250905_191305297_iOS.heic to 20250905_191305297_iOS.png
+Converted 20250905_192253420_iOS.heic to 20250905_192253420_iOS.png
+Converted 20250906_123810137_iOS.heic to 20250906_123810137_iOS.png
+Converted 20250906_123958934_iOS.heic to 20250906_123958934_iOS.png
+Converted 20250906_123959292_iOS.heic to 20250906_123959292_iOS.png
+Converted 20250906_124112351_iOS.heic to 20250906_124112351_iOS.png
+Converted 20250906_124201881_iOS.heic to 20250906_124201881_iOS.png
+Converted 20250906_124812716_iOS.heic to 20250906_124812716_iOS.png
